9feda7bcac00d06760ff956136b9c70f57c6a867
rodzena <48984657+rodzena@users.noreply.github.com>
2019-06-09 02:54:27 +0200

    naprawiony count, podział na funkcje rules

--- logic/Generation.java
+++ logic/Generation.java
@@ -3,22 +3,22 @@ public class Generation {
     private int y;
     private int genNumber;
 
-    public Generation(FileRead read, Config config){
+    public Generation(FileRead read, Config config) {
         this.x = read.getX();
         this.y = read.getY();
         this.genNumber = config.getGenNumber();
 
     }
 
-    public Cell[][] generate(FileRead read){
+    public Cell[][] generate(FileRead read) {
         Grid grid = new Grid(this.x, this.y);
         Rules rules = new Rules();
         System.out.println();
-        Cell[][] cells = grid.createInitial(x,y, read.getTemp());
-        
+        Cell[][] cells = grid.createInitial(x, y, read.getTemp());
+
 
         for (int k = 0; k < genNumber; k++) {
-            rules.updateNeighbourCount(cells,x,y);
+            rules.updateNeighbourCount(cells, x, y);
             rules.changeStates(cells, x, y);
 
 
@@ -40,51 +40,6 @@ public class Generation {
         }
 
         return cells;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        //int x =
-        //Grid grid = new Grid(x,y);
     }
-
-//    public void createInitial (int x, int y, String[][] input){
-//
-//        Grid grid = new Grid(10,10);
-//
-//        for(int i = 0; i < x; i ++){
-//            for (int j = 0; i < y; j++){
-//                if(input[i][j]=="E"){
-//                    grid.grid[i][j] = new Empty();
-//                }
-//                else if(input[i][j]=="H"){
-//                    grid.grid[i][j] = new Head();
-//                }
-//                else if(input[i][j] == "T"){
-//                    grid.grid[i][i] = new Tail();
-//                }
-//                else {
-//                    grid.grid[i][j] = new Conductor();
-//                }
-//
-//            }
-//        }
-//    }
-
-
 }
 
--- logic/Main.java
+++ logic/Main.java
@@ -3,12 +3,12 @@ public class Main {
 
     public static void main(String[] args) {
         //te rzeczy idą z gui
-        String nazwapliku = "plik.txt";
-        String nazwawyjsciowego = "plikout.txt";
+        String plikin = "plik.txt";
+        String plikout = "plikout.txt";
         int genNumber = 10;
 
 
-        Config config = new Config(5,10, nazwapliku, nazwawyjsciowego);
+        Config config = new Config(5,10, plikin, plikout);
         FileRead read = new FileRead(config);
 
 
@@ -18,13 +18,6 @@ public class Main {
             read.printGrid();
             Generation gen = new Generation(read, config);
             gen.generate(read);
-
-//            //to do wyjebania potem
-//            for (int i = 0; i < genNumber; i++) {
-//                System.out.println();
-//                System.out.println("Gen #" + i);
-//                gen.generate(read);
-//            }
         }
 
     }
